a2bea36028dbb6a4f59273e8bed8f94c0eef709d
pistaacademy <pista.academy@gmail.com>
2023-06-07 06:28:54 +0330

    Session-2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
 
+import TopNav from './components/TopNac';
+
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
+    <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
--- src/booking/Home.jsx
+++ src/booking/Home.jsx
@@ -1,6 +1,9 @@
+import { useSelector, useStore } from "react-redux";
+
 const Home = () => {
+    const {user} = useSelector((state)=>({...state}));
     return (
-        <div className="container-fluid h1 p-5 text-center">Home Page</div>
+        <div className="container-fluid h1 p-5 text-center">Home Page {JSON.stringify(user)}</div>
     )
 }
 
