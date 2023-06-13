583a560d338b6ed8dbb354032e197dd3d37f5b2a
pistaacademy <pista.academy@gmail.com>
2023-06-13 06:49:56 +0330

    Session-6

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express  from "express";
 import fs from "fs";
+const morgan = require("morgan");
+import cors from "cors";
+import mongoose from "mongoose";
+
+
+require("dotenv").config();
 
 const app = express();
 
+// db connection
+mongoose.connect(process.env.DATABASE,{})
+.then(() => console.log("DB Connected"))
+.catch(err => console.log("DB Not Connected: ", err))
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+
 fs.readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
+const port = process.env.PORT || 8000;
 
-app.listen(8000, () => console.log('Server is running on port 8000'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
