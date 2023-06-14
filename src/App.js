545c9a137cc44112ca9c12eb36134aa4dc3c021e
pistaacademy <pista.academy@gmail.com>
2023-06-14 05:55:23 +0330

    Sesson-7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
 
-import TopNav from './components/TopNac';
+import TopNav from './components/TopNav';
 
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,6 +1,67 @@
+import { useState } from "react";
+
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ name, email, password });
+    }
+
+    const registerForm = () => {
+        return (
+            <form onSubmit={handleSubmit} className="mt-3">
+                <div className="form-group mb-3">
+                    <label className="form-label">Your Name</label>
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        placeholder="Enter Your Name ..." 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <label className="form-label">Your Email</label>
+                    <input 
+                        type="email" 
+                        className="form-control"
+                        placeholder="Enter Your Email ..." 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div className="form-group mb-3">
+                    <label className="form-label">Your Password</label>
+                    <input 
+                        type="password" 
+                        className="form-control"
+                        placeholder="Enter Your Password ..." 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
     return (
-        <div className="container-fluid h1 p-5 text-center">Register Page</div>
+        <>
+            <div className="container-fluid bg-secondary p-5 text-center">
+                <h1>Register</h1>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        {registerForm()}
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
