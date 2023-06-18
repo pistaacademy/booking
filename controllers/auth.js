2217a64499436bacfbf8e87dd9002858249da05b
pistaacademy <pista.academy@gmail.com>
2023-06-18 06:20:25 +0330

    Session-11

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,22 @@
-export const showMessage = (req, res)=> {
-    res.status(200).send(`This is your message: ${req.params.message}`);
-}
+import User from "../model/user";
 
-export const register = (req, res) => {
-    console.log(req.body)
+export const register = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    if(!name) return res.status(400).send("Name is Required");
+    if(!password || password.length < 6) return res.status(400).send("Password is required and should be min 6 character long");
+
+    let userExist = await User.findOne({ email }).exec();
+    if (userExist) return res.status(400).send('Email is taken');
+
+    const user = new User(req.body);
+    try{
+        await user.save();
+        console.log("User Created", user);
+        return res.json({ ok: true });
+    }
+    catch{
+        console.log("Create User Failed", err);
+        return res.status(400).send("Error. Try again.")
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-import { showMessage,register } from "../controllers/auth";
+import { register } from "../controllers/auth";
 
-router.get("/:message", showMessage);
+
 router.post("/register", register)
 
 module.exports = router;
