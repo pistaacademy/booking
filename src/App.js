083578051b5536763f03731cc32b0f48a7d72c56
pistaacademy <pista.academy@gmail.com>
2023-06-18 07:07:25 +0330

    Session-12

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Register from './auth/Register';
 
 import TopNav from './components/TopNav';
 
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
     <TopNav />
+    <ToastContainer position="top-center" theme="dark" />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 
 import RegisterForm from "../components/RegisterForm";
 
-const Register = () => {
+import { toast } from 'react-toastify';
+
+
+const Register = ({history}) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -11,14 +14,19 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`http://localhost:8000/api/register`, {
+            const res = await axios.post(`${process.env.REACT_APP_API}/register`, {
             name,
             email,
             password,
         });
+
         console.log(res);
+        toast.success("Register success.Please Login.");
+        history.push("/login");
+
         } catch (err) {
             console.log(err)
+            if(err.response.status === 400) toast.error(err.response.data)
         }
     }
 
@@ -27,6 +35,9 @@ const Register = () => {
             <div className="container-fluid bg-secondary p-5 text-center">
                 <h1>Register</h1>
             </div>
+
+            
+
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
